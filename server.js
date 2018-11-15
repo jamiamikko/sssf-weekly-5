373fa5947208dcd80186fdf99a58a4cc421ac6e5
jamiamikko <jamiamikko@gmail.com>
2018-11-15 20:56:46 +0200

    Connect to io

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.use((req, res, next) => {
 
 const port = process.env.PORT || 3000;
 
-const server = https.createServer(app).listen(port, () => {
+const server = app.listen(port, () => {
   console.log('Listening to port' + port);
 });
 
