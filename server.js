08bfe6046a55c96fdab791179c56dd9e3c479343
jamiamikko <jamiamikko@gmail.com>
2018-11-15 20:15:18 +0200

    Enable cors

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const passport = require('passport');
 const handlebars = require('express-handlebars');
 const socket = require('socket.io');
 const helmet = require('helmet');
+const cors = require('cors');
 
 require('./passport')(passport);
 
@@ -42,6 +43,7 @@ app.use(
   })
 );
 
+app.use(cors());
 app.use(helmet());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -49,30 +51,6 @@ app.use(passport.session());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 const sslkey = fs.readFileSync('./config/ssl-key.pem');
 const sslcert = fs.readFileSync('./config/ssl-cert.pem');
